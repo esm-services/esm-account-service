dda948bbd25403ee995b5bdc3c556e01a69e8aa0
Durgesh RAI <drai548@gmail.com>
2017-07-03 01:09:44 +0530

    renamed the main class

--- src/main/java/com/esm/account/service/resource/AccountResource.java
+++ src/main/java/com/esm/account/service/resource/AccountResource.java
@@ -1,10 +1,12 @@
 package com.esm.account.service.resource;
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class AccountResource {
 
 	@GetMapping("/message")
