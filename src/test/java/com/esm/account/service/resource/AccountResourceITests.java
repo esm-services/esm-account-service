0d0ecff965b5b18643cf84850806ca1163b8ea71
Durgesh RAI <drai548@gmail.com>
2017-08-17 23:57:45 +0530

    fixed the security issue for integration tets

--- src/test/java/com/esm/account/service/resource/AccountResourceITests.java
+++ src/test/java/com/esm/account/service/resource/AccountResourceITests.java
@@ -1,58 +1,43 @@
 package com.esm.account.service.resource;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.junit.Rule;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.client.test.OAuth2ContextConfiguration;
-import org.springframework.security.oauth2.client.test.OAuth2ContextSetup;
-import org.springframework.security.oauth2.client.test.RestTemplateHolder;
-import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestOperations;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import com.esm.account.service.ESMAccountServiceApplication;
+import com.esm.account.service.security.WithMockCustomUser;
 
 @RunWith(SpringRunner.class)
-@ActiveProfiles("test")
-@SpringBootTest(classes = ESMAccountServiceApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
-public class AccountResourceITests implements RestTemplateHolder {
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class AccountResourceITests{
 
-	@Value("http://localhost:${local.server.port}")
-	protected String host;
+	@Autowired
+	private WebApplicationContext context;
 
-	@Getter
-	@Setter
-	private RestOperations restTemplate = new RestTemplate();
+	private MockMvc mockMvc;
 
-	@Rule
-	public OAuth2ContextSetup context = OAuth2ContextSetup.standard(this);
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).alwaysDo(print()).build();
+	}
 
 	@Test
-	@OAuth2ContextConfiguration(UserDetails.class)
-	public void testHelloUser() {
-		ResponseEntity<String> entity = restTemplate.getForEntity(host + "/message", String.class);
-		assertTrue(entity.toString(), entity.getStatusCode().is2xxSuccessful());
-		assertEquals("Welcome to OAuth2.0", entity.getBody());
+	@WithMockCustomUser(username = "user")
+	public void testHelloUser() throws Exception {
+		mockMvc.perform(get("/message")).andExpect(status().isOk()).andExpect(content().string("Welcome to OAuth2.0"));
 	}
 }
 
-class UserDetails extends ResourceOwnerPasswordResourceDetails {
-	public UserDetails(final Object obj) {
-		AccountResourceITests it = (AccountResourceITests) obj;
-		setAccessTokenUri(it.host + "/oauth/token");
-		setClientId("myclientwith");
-		setUsername("user");
-		setPassword("password");
-	}
-}
+
---
+++ src/test/java/com/esm/account/service/resource/AccountResourceUTests.java
@@ -0,0 +1,28 @@
+package com.esm.account.service.resource;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.esm.account.service.security.WithMockCustomUser;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class AccountResourceUTests {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	@WithMockCustomUser(username = "user")
+	public void testHelloUserWithRole() throws Exception {
+		mvc.perform(get("/message")).andExpect(status().isOk()).andExpect(content().string("Welcome to OAuth2.0"));
+	}
+}
